ae71821cdc9bc16fbde84e7b5566d9e29b496229
n-eaton <natallia.eaton@gmail.com>
2023-08-17 16:14:58 -0500

    commit

--- authentication/views.py
+++ authentication/views.py
@@ -1,7 +1,26 @@
 from django.shortcuts import render
 from django.views import View
+import json
+from django.http import JsonResponse
+from django.contrib.auth.models import User
+
 # Create your views here.
 
 class RegistrationView(View):
     def get(self, request):
         return render( request, 'auth/register.html')
+    
+class UserNameValidation(View):
+    def post(self, request):
+        data = json.loads(request.body)
+        username = data['username']
+        
+        if not str(username).isalnum():
+            return JsonResponse({'username_error': 'username should only contain alphanumeric characters'}, status=400)
+        
+
+        if User.objects.filter(username=username).exists():
+            return JsonResponse({'username_error': 'The username is taken. Please choose another one'}, status=409)
+        
+        
+        return JsonResponse({'username_valid': True})
